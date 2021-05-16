6afc4a65490ff21304d63f6211b99815ad93cfd1
Aaron Fisk <axelfisk12@gmail.com>
2021-05-15 22:50:46 -0300

    "Hello"

--- src/murff/Main.java
+++ src/murff/Main.java
@@ -2,7 +2,7 @@ package murff;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, my name is Nicole.");
+    public static void main(String[] args) { System.out.println("Hello, my name is Nicole.");
+        System.out.println("Hello Nicole, my name is Aaron.");
     }
 }
